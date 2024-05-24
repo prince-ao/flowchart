d7d6d765b4922e89a01b7e7a032149bee51283ce
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2024-05-24 13:22:10 -0400

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,176 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center text-black bg-[#FAF8F5]">
+      {/* navbar */}
+      <div className="navbar bg-[#FAF8F5] text-black">
+        <div className="navbar-start">
+          {/* <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div> */}
+          {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
+          <img src="logo.svg" alt="logo" className="w-60 ml-20" />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <details>
+                <summary>Flowchart</summary>
+                <ul className="p-2 bg-[#FAF8F5]">
+                  <li>
+                    <a>2019 - 2020 </a>
+                  </li>
+                  <li>
+                    <a>2021 - 2022 </a>
+                  </li>
+                  <li>
+                    <a>2021 - 2022 </a>
+                  </li>
+                  <li>
+                    <a>FAQ </a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Degree map</summary>
+                <ul className="p-2 bg-[#FAF8F5]">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-full xs:w-3/4 carousel  border border-gray-300 rounded-lg  lg:flex">
+        <div id="item1" className=" justify-between carousel-item w-full flex">
+          <div class="ml-10 mt-40  font-bold text-xl ">
+            Unlock Your Potential and Navigate Your Way to Success{" "}
+          </div>
+
+          <img src="edimage.png" className=" w-50 h-45 " />
+        </div>
+        <div id="item2" class="carousel-item w-full ">
+          TBD{" "}
+        </div>
+        <div id="item3" class="carousel-item w-full ">
+          TBD
+        </div>
+        <div id="item4" class="carousel-item w-full ">
+          TBD
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <div class="flex justify-center w-full py-2 gap-2 mb-10">
+        <a href="#item1" class="btn btn-xs">
+          1
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+        <a href="#item2" class="btn btn-xs">
+          2
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
+        <a href="#item3" class="btn btn-xs">
+          3
         </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+        <a href="#item4" class="btn btn-xs">
+          4
         </a>
       </div>
+      <div className="w-full flex justify-center text-2xl mb-20">
+        Some Text here
+      </div>
+      <div className="w-full flex justify-center">
+        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
+          <h2 className="card-title">Create Your own flowchart </h2>
+          <figure>
+            <img src="flowchart.svg" alt="degree map" className="mt-5" />
+          </figure>
+          <div className="card-body">
+            <p> Plan your path to success</p>
+          </div>
+        </div>
+   
+        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
+          <h2 className="card-title">4 year Degree Map</h2>
+          <figure>
+            <img src="degree.svg" alt="degree map" className="mt-5" />
+          </figure>
+          <div className="card-body">
+            <p> Plan your path to success</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-20 w-full flex justify-center">
+        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
+          <h2 className="card-title">flowchart </h2>
+          <figure>
+            <img src="flowchart.svg" alt="degree map" className="mt-5" />
+          </figure>
+          <div className="card-body">
+            <p> Plan your path to success</p>
+          </div>
+        </div>
+   
+        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
+          <h2 className="card-title">4 year Degree Map</h2>
+          <figure>
+            <img src="degree.svg" alt="degree map" className="mt-5" />
+          </figure>
+          <div className="card-body">
+            <p> Plan your path to success</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+      
+      <img src="img.svg"/>
+
+      </div>
+     {/* footer  */}
+      <footer class=" mt-10 w-full bg-gray-800 py-6">
+        <div class="container mx-auto flex flex-col items-center">
+          <nav class=" flex flex-wrap justify-center">
+            <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
+              Home
+            </a>
+            <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
+              About
+            </a>
+            <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
+              Services
+            </a>
+            <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
+              Contact
+            </a>
+          </nav>
+        </div>
+      </footer>
     </main>
   );
 }
