1b91080703b75733bf0a12198f2ad60ad685f9a8
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2024-05-29 23:52:18 -0400

    theme added, homepage updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,54 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-black bg-[#FAF8F5]">
+    <main className="flex min-h-screen flex-col items-center text-black ">
       {/* navbar */}
-      <div className="navbar bg-[#FAF8F5] text-black">
+      <div className="navbar bg-primary text-white">
         <div className="navbar-start">
-          {/* <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Home</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div> */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
           {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
           <img src="logo.svg" alt="logo" className="w-60 ml-20" />
         </div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-center hidden lg:flex ">
           <ul className="menu menu-horizontal px-1">
             <li>
               <a>Home</a>
@@ -32,7 +56,7 @@ export default function Home() {
             <li>
               <details>
                 <summary>Flowchart</summary>
-                <ul className="p-2 bg-[#FAF8F5]">
+                <ul className="p-2  text-black">
                   <li>
                     <a>2019 - 2020 </a>
                   </li>
@@ -51,7 +75,7 @@ export default function Home() {
             <li>
               <details>
                 <summary>Degree map</summary>
-                <ul className="p-2 bg-[#FAF8F5]">
+                <ul className="p-2 bg-background text-black">
                   <li>
                     <a>Submenu 1</a>
                   </li>
@@ -66,18 +90,43 @@ export default function Home() {
             </li>
           </ul>
         </div>
+        <div className="navbar-end">
+          <a className="btn btn-secondary">Get Started</a>
+        </div>
       </div>
 
       <div className="w-full xs:w-3/4 carousel  border border-gray-300 rounded-lg  lg:flex">
         <div id="item1" className=" justify-between carousel-item w-full flex">
-          <div class="ml-10 mt-40  font-bold text-xl ">
+          <div class="ml-10 sm:mt-40 mt-10 font-bold sm:text-xl ">
             Unlock Your Potential and Navigate Your Way to Success{" "}
+            <div>
+              {" "}
+              <a className="btn btn-secondary">Get Started</a>
+            </div>
           </div>
 
-          <img src="edimage.png" className=" w-50 h-45 " />
+          <img
+            src="main.png"
+            className=" w-64 sm:w-3/4 h-64 sm:h-96"
+            alt="description"
+          />
+          {/* sm:w-24 sm:h-24 */}
+          {/* <img src="edimage.png" className=" w-25 h-25  xs:w-25 xs:h-10 " /> */}
         </div>
         <div id="item2" class="carousel-item w-full ">
-          TBD{" "}
+        <div class="ml-10 sm:mt-40 mt-10 font-bold sm:text-xl ">
+        Map your academic path with ease using our online flowchart tool <div>
+              {" "}
+              <Link href="/flowchart/create">
+              <a className="btn btn-secondary">Create Your Own Flowchart</a> 
+               </Link>
+            </div>
+          </div>
+          <img
+            src="file.png"
+            className=" w-64 sm:w-3/4 h-64 sm:h-96"
+            alt="description"
+          />
         </div>
         <div id="item3" class="carousel-item w-full ">
           TBD
@@ -100,59 +149,72 @@ export default function Home() {
           4
         </a>
       </div>
-      <div className="w-full flex justify-center text-2xl mb-20">
+      {/* <div className="hidden lg:flex  text-2xl mb-20">
         Some Text here
-      </div>
-      <div className="w-full flex justify-center">
-        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
-          <h2 className="card-title">Create Your own flowchart </h2>
-          <figure>
-            <img src="flowchart.svg" alt="degree map" className="mt-5" />
-          </figure>
-          <div className="card-body">
-            <p> Plan your path to success</p>
-          </div>
-        </div>
-   
-        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
-          <h2 className="card-title">4 year Degree Map</h2>
-          <figure>
-            <img src="degree.svg" alt="degree map" className="mt-5" />
-          </figure>
-          <div className="card-body">
-            <p> Plan your path to success</p>
-          </div>
-        </div>
-      </div>
+      </div> */}
 
-      <div className="mt-20 w-full flex justify-center">
-        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
-          <h2 className="card-title">flowchart </h2>
-          <figure>
-            <img src="flowchart.svg" alt="degree map" className="mt-5" />
+        <div className="hidden lg:flex  items-center justify-center gap-4 ">
+        <div className="w-full flex flex-col items-center ">
+
+          <figure className="w-full flex justify-center">
+            <img src="flowchart.png" alt="flowchart" className="w-3/4" />
           </figure>
-          <div className="card-body">
-            <p> Plan your path to success</p>
-          </div>
         </div>
-   
-        <div className="card w-96  shadow-xl w-1/2 flex flex-col items-center">
-          <h2 className="card-title">4 year Degree Map</h2>
-          <figure>
-            <img src="degree.svg" alt="degree map" className="mt-5" />
-          </figure>
-          <div className="card-body">
-            <p> Plan your path to success</p>
-          </div>
+        <div className="w-full flex flex-col ">
+          <div className="">
+          <p className="text-bold  text-4xl">Flowchart </p>
+            <p  className="text-bold  text-2xl">Plan your path to success</p>
+            <Link href="/flowchart/create">
+             <button className="btn btn-secondary mt-5">Create Your Own Flowchart</button>
+          </Link> </div>
         </div>
       </div>
-
-      <div className="">
+      <div className="hidden lg:flex  items-center justify-center gap-4 ">
+        <div className="w-full flex flex-col items-center ">
+   <div className="">
+            <p className="text-bold  text-4xl">Degree Works</p>
+            <p className="text-bold  text-2xl">Look into your four year degree map</p>
+            <Link href="/flowchart/create">
+             <button className="btn btn-secondary mt-5">Create Your Own Flowchart</button>
+          </Link> 
+          </div>
       
-      <img src="img.svg"/>
-
+        </div>
+        <div className="w-full  flex flex-col items-center  ">  
+          <figure className="w-full flex justify-center">
+            <img src="degree.png" alt="flowchart" className="w-3/4" />
+          </figure>
+       
+        </div>
       </div>
-     {/* footer  */}
+
+
+
+      
+      <div className="w-84 lg:hidden flex flex-col sm:flex-row items-center justify-center gap-4 p-6 ">
+        <div className="card shadow-xl w-full sm:w-1/2 flex flex-col items-center p-6 bg-white rounded-lg">
+          <h2 className="card-title text-center mb-4">
+            Create Your Own Flowchart
+          </h2>
+          <figure className="w-full flex justify-center">
+            <img src="flowchart.svg" alt="flowchart" className="mt-5" />
+          </figure>
+          <div className="card-body">
+            <p>Plan your path to success</p>
+          </div>
+        </div>
+        <div className="card shadow-xl w-full sm:w-1/2 flex flex-col items-center p-6 bg-white rounded-lg">
+          <h2 className="card-title text-center mb-10">4 Year Degree Map</h2>
+          <figure className="w-full flex justify-center">
+            <img src="flowchart.svg" alt="flowchart" className="mt-5" />
+          </figure>
+          <div className="card-body">
+            <p>Plan your path to success</p>
+          </div>
+        </div>
+      </div>
+
+      {/* footer  */}
       <footer class=" mt-10 w-full bg-gray-800 py-6">
         <div class="container mx-auto flex flex-col items-center">
           <nav class=" flex flex-wrap justify-center">
