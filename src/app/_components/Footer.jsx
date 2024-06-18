775e3d1f6662ed41e731048011820a75f4cfefd6
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2024-06-18 01:02:09 -0400

    page/year updated

--- src/app/_components/Footer.jsx
+++ src/app/_components/Footer.jsx
@@ -1,15 +1,12 @@
 export default function Footer() {
   return (
-    <footer class="mt-10 w-full h-[80px] bg-gray-800 py-6 flex justify-center items-center">
-      <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
-        About
-      </a>
-      <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
-        Services
-      </a>
-      <a href="#" class="text-gray-300 hover:text-white px-4 py-2">
-        Contact
-      </a>
+    <footer class="mt-10 w-full h-[80px] bg-gray-800 py-6 flex flex-col justify-center items-center text-[white]">
+   <div className="flex gap-4">
+    <a href="#" className="hover:text-white px-4 py-2">About</a>
+    <a href="#" className="hover:text-white px-4 py-2">Contact</a>
+  </div>
+      <div className="text-[white]">THIS IS INTENDED TO BE USED ONLY AS A GUIDE </div>
+
     </footer>
   );
 }
